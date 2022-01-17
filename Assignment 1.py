ca1663240ce3feba4b80a4337a522e4eef8f52ff
KARSarma <87109583+KARSarma@users.noreply.github.com>
2022-01-17 18:42:32 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Assignment 1
+
+
+# In[ ]:
+
+
+#question 1
+
+#l=[*,'hello',-87-8,-,/,+,6]
+#*,-,/,+ are mathematical expressions
+
+#hello',-7.8,6 are string and values 
+
+
+# In[ ]:
+
+
+#question 2
+
+#A variable is created when we first assign a value to it in a program 
+#ex: a=90
+    
+#anything assigned to vaiable with single or double quotes 
+#a='10'
+
+#hence a varaible is used to store information and string is a type of information.
+
+
+# In[ ]:
+
+
+#question 3
+
+#numeric datatype: used to store numeric datatpes
+
+#boolean datatype: used to store boolean values(true/false)
+
+#sequence datatype: used to store data in squence(list,tuple,range)
+
+
+# In[ ]:
+
+
+#question 4
+
+#a combination of operators and operands that is interpreted to produce some other value.
+
+#Expressions are representations of value.
+
+
+# In[ ]:
+
+
+#question 5
+
+#Evaluation of an expression always Produces or returns a result value. Execution of a statement may or may not produces or displays a result value.
+
+
+# In[2]:
+
+
+#question 6
+bacon = 22
+bacon +1
+
+#output would be 23
+
+
+# In[3]:
+
+
+#question 7
+'spam'+'spamspam'
+'spam'*3
+
+#output is 'spamspamspam'
+
+
+# In[4]:
+
+
+#question 8 
+
+#As variable name can not start with a number.
+
+
+# In[ ]:
+
+
+#question 9
+
+#int() for integer value
+
+# float() for floating-point number
+
+# str() for string
+
+
+# In[11]:
+
+
+#question 10
+
+#a='i have eaten'+99+'burritos'
+
+#as we can only cocatenate string to string and not integer 
+
+a='i have eaten'+" 99 " +'burritos'
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
